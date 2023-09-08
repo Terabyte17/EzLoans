e89b3e895c0dedc76a051553d98da7cc8bc4ceaa
unknown <yash.sahijwani.ece19@itbhu.ac.in>
2023-09-08 15:02:34 +0530

    base commit

---
+++ src/main/java/com/wellsfargo/ezloans/EzLoansApplication.java
@@ -0,0 +1,13 @@
+package com.wellsfargo.ezloans;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EzLoansApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EzLoansApplication.class, args);
+	}
+
+}
