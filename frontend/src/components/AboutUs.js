dbf0e41e0fa12bfcd051b8a1b52d36db636fca96
pilgrim <sanchitgupta.072@gmail.com>
2023-09-14 19:38:44 +0530

    Landing page with header footer and navbar

---
+++ frontend/src/components/AboutUs.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AboutUs = () => {
+    return(
+        <div>
+            <h2 style={{color:'brown'}}>About Us</h2>
+        </div>
+    )
+}
+
+export default AboutUs
---
+++ frontend/src/components/Registration.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Registration = () => {
+    return(
+        <div>
+            <h2 style={{color:'brown'}}>Dealer Registration</h2>
+        </div>
+    )
+}
+
+export default Registration
